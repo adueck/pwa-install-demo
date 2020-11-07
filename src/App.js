240d4ef7a2667e0d07510e6c114cb0898df8b72e
adueck <adueck@users.noreply.github.com>
2020-11-07 19:31:15 +0400

    more info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-let deferredPrompt;
 const repo = "https://github.com/adueck/pwa-install-demo";
-
+let deferredPrompt;  
+    
 function App() {
   const [installable, setInstallable] = useState(false);
+  const [installed, setInstalled] = useState(false);
 
   useEffect(() => {
     window.addEventListener("beforeinstallprompt", (e) => {
@@ -16,27 +17,32 @@ function App() {
       // Update UI notify the user they can install the PWA
       setInstallable(true);
     });
+
     window.addEventListener('appinstalled', () => {
       // Log install to analytics
       console.log('INSTALL: Success');
     });
+    
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      setInstalled(true);
+    }
   }, []);
 
   const handleInstallClick = (e) => {
-    // Hide the app provided install promotion
-    setInstallable(false);
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
-      } else {
-        console.log('User dismissed the install prompt');
-      }
-    });
+      // Hide the app provided install promotion
+      setInstallable(false);
+      // Show the install prompt
+      deferredPrompt.prompt();
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the install prompt');
+        } else {
+          console.log('User dismissed the install prompt');
+        }
+      });
   };
-
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -45,6 +51,9 @@ function App() {
           <button className="install-button" onClick={handleInstallClick}>
             INSTALL ME
           </button>
+        }
+        {(!installable && !installed) &&
+          <p>Sorry, your browser doesn't support the internal install button</p>
         }
         <p>
           <a href={repo} className="App-link">View source on GitHub</a>
